0b27c0da1531502651219d382e9fe1f98c1b9108
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 16:10:26 +0330

    Initialize Model Container in VM

--- Watchlist SwiftUI MasterClass/Watchlist_SwiftUI_MasterClassApp.swift
+++ Watchlist SwiftUI MasterClass/Watchlist_SwiftUI_MasterClassApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct Watchlist_SwiftUI_MasterClassApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Movie.self)
         }
     }
 }
