69da20a2f28595a018d09de10f029f670ab3c152
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 15:41:43 +0330

    Initial Commit

---
+++ Watchlist SwiftUI MasterClass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Watchlist SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 31/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Watchlist SwiftUI MasterClass/Watchlist_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watchlist_SwiftUI_MasterClassApp.swift
+//  Watchlist SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 31/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Watchlist_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
