5174d27451f0fada6a172766bbd4ab5032e24835
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 16:13:23 +0330

    Add SwiftData and Add our Custom EmptyListView to ContentView

--- Watchlist SwiftUI MasterClass/View/ContentView.swift
+++ Watchlist SwiftUI MasterClass/View/ContentView.swift
@@ -6,19 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    // MARK: - Properties
+    
+    @Environment(\.modelContext) var modelContext
+    @Query private var movies: [Movie]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        List {
+            
+        } // List
+        .overlay {
+            if movies.isEmpty {
+                EmptyListView()
+            }
+        } // Overlay
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Movie.self, inMemory: true)
 }
