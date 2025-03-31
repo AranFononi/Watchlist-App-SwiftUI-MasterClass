216d987a145a60d1d4d75bb773c0465b7a5beb73
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 16:09:27 +0330

    Create TabView Inside EmptyListView and Add PageTabView()

--- Watchlist SwiftUI MasterClass/View/EmptyListView.swift
+++ Watchlist SwiftUI MasterClass/View/EmptyListView.swift
@@ -9,20 +9,37 @@ import SwiftUI
 
 struct EmptyListView: View {
     var body: some View {
-        Circle()
-            .fill(.blue.gradient)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
-            .frame(width: 180)
-            .overlay {
-                Image(systemName: "movieclapper")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100)
-                    .foregroundStyle(Color(UIColor.secondarySystemBackground))
-                    .padding(.top, -6)
+        ContentUnavailableView {
+            Circle()
+                .fill(.blue.gradient)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
+                .frame(width: 180)
+                .overlay {
+                    Image(systemName: "movieclapper")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100)
+                        .foregroundStyle(Color(UIColor.secondarySystemBackground))
+                        .padding(.top, -6)
+                }
+                .padding(.bottom, 90)
+        } description: {
+            GroupBox {
+                TabView {
+                    PageTabView(icon: "1.circle", description: "Add Some Movies To Wath Later")
+                        .padding(.bottom, 36)
+                    PageTabView(icon: "2.circle", description: "Later On You Can Also Choose A Random Film To Watch")
+                        .padding(.bottom, 36)
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .frame(minWidth: 0, maxWidth: 560, minHeight: 120, maxHeight: 180)
+                
+                
+                
             }
+        }
     }
 }
 
